3818803350fa7da56f82ea2f73f6de4081822d03
mouyj <mouyj@2345.com>
2020-07-05 11:16:51 +0800

    项目appconst

---
+++ domains/consts/AppConst.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miaoyijin
+ * Date: 2020/7/5
+ * Time: 11:16 AM
+ */
+
+class AppConst {
+
+}
--- public/web.php
+++ public/web.php
@@ -17,16 +17,16 @@ if (!empty($_SERVER['HTTP_ORIGIN'])) {
     $CORSDomain = $_SERVER['HTTP_ORIGIN'];
 }
 if ($isDev) {
-    //如果沒有接入公司网关则$is_dev判断需要去掉
-    $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
-    header('Access-Control-Allow-Origin: ' . $CORSDomain);
+    //如果沒有接入公司网关则$isDev判断需要去掉
+    $httpScheam = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+    header('Access-Control-Allow-Origin: ' . $httpScheam . $CORSDomain);
     header('Access-Control-Allow-Headers: NOT,*');
     header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Max-Age: 3600");
 }
 if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
-    exit('ok');
+    exit('cors ok');
 }
 
 /*
